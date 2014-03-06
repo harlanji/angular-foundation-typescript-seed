b701d7621aed76ffaba9e0d0db993df5abc5787d
root <root@node-dev.(none)>
2014-03-06 04:43:12 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+//var server = express.createServer();
+// express.createServer()  is deprecated. 
+var server = express(); // better instead
+server.configure(function(){
+  server.use('/media', express.static(__dirname + '/media'));
+  server.use('/bc', express.static(__dirname + '/bower_components'));
+  server.use(express.static(__dirname + '/public'));
+});
+
+server.listen(3000);
